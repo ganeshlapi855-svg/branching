1cabbe0a6eb6749542375af5e35e5cf3b64234ec
ganesh <ganeshlapi855@gmail.com>
2026-09-04 09:45:36 +0530

    saving the file to second branch

---
+++ userinfo.py
@@ -0,0 +1,15 @@
+print("Enter User Details")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+age = input("Enter your age: ")
+email = input("Enter your email: ")
+phone = input("Enter your phone number: ")
+
+print("\n--- User Details ---")
+print("Name:", first_name + " " + last_name)
+print("Age:", age)
+print("Email:", email)
+print("Phone:", phone)
+
+
