5cb9ed94df4bb2f6063428b9c56f31f6cfd3d60e
ganesh <ganeshlapi855@gmail.com>
2026-09-04 10:20:17 +0530

    saving the file to third branch

---
+++ user.py
@@ -0,0 +1,8 @@
+print("Enter User Details")
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+
+print("\n--- User Details ---")
+print("Name:", first_name + " " + last_name) 
