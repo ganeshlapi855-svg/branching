60bb4862abc0df436b274ce0622c069e8fbb2a84
ganesh <ganeshlapi855@gmail.com>
2026-09-04 09:01:15 +0530

    saving the file

---
+++ user_details.py
@@ -0,0 +1,12 @@
+print("Enter User Details")
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+email = input("Enter your email: ")
+phone = input("Enter your phone number: ")
+
+print("\n--- User Details ---")
+print("Name:", name)
+print("Age:", age)
+print("Email:", email)
+print("Phone:", phone)
